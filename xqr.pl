0e9188dbf41226e2fd80eed30c811be02f34563d
Barush <barush@BSkrivankova-UX32VD.(none)>
2013-03-15 22:00:59 +0100

    hotovo, odevzdana verze

--- xqr.pl
+++ xqr.pl
@@ -1,10 +1,17 @@
 #! /usr/bin/perl
-#use strict;
+
+########################################################################
+#	XQR.pl
+#	autor: Barbora Skrivankova, xskriv01@stud.fit.vutbr.cz
+#	datum: 15.3.2012
+#	popis: projekt do IPP
+#		provadi jednoduche select-like dotazy nad xml daty
+########################################################################
 use Getopt::Long qw(Configure GetOptions);
 use XML::LibXML;
 use Data::Dumper;
 use strict;
-#use warnings;
+use warnings;
 
 #Funkce pro validaci xml dotazu pomoci rekurzivniho sestupu
 sub validateQuery($);
@@ -39,24 +46,26 @@ my $helpmsg = "Pouziti: ./xqr.pl
 my %params;		#parametry nactene ze vstupu + ze zpracovani dotazu
 my $xmlFile;    #struktura z knihovny libXML uchovavajici data nactena z xml souboru
 	
+#nacteni parametru prikazove radky
 if(!GetOptions(\%params, "--help", "--input=s", "--output=s", "--query=s", 
 	"--qf=s", "-n", "--root=s") ){
 	print STDERR "Interni chyba pri zpracovani parametru.\n";
 	exit 1;
 }
 
+#overeni kolize neslucitelnych parametru
 if($params{"help"}){
 	print $helpmsg; 
 	(scalar keys %params > 1) and exit 1;
 	exit 0;
 }
-
 if($params{"qf"} and $params{"query"}){
 	print STDERR "Chybna kombinace parametru.\n";
 	print STDERR $helpmsg;
 	exit 1;
 }
 
+#nacteni dotazu v pripade, ze byl uveden v souboru
 if($params{"qf"}){
 	if(!open(QUERYF, $params{"qf"})){
 		print STDERR "Doslo k chybe pri otevirani query file.\n";
@@ -66,12 +75,16 @@ if($params{"qf"}){
 	close QUERYF;
 }
 
+#volani overeni validity dotazu dle bkg ze zadani
 if(validateQuery(\%params)){
 	print STDERR "Dotaz nebyl zadan v podporovanem formatu. \n";
 	exit 10;
 }
+
+#nacteni xml ze souboru
 $xmlFile = readXML(\%params);
 
+#vytvoreni xml dokumentu a pripadne smazani jeho hlavicky dle parametru -n
 my $document = XML::LibXML->createDocument( "1.0", "utf-8" );
 if($params{"n"}){
 	$document = "";
@@ -80,13 +93,18 @@ else{
 	$document = $document->toString();
 }
 
+#vlozeni obalujiciho elementu do dokumentu
 if($params{"root"}){
 	$document .= "<".$params{"root"}.">";
 }
 
+#citac poctu zapsanych elementu
 my $counter = 1;
+
+#volani funkci pro zpracovani dotazu
 if($params{"fromElement"} eq "ROOT"){
-	if($params{"fromAttribute"}){		
+	if($params{"fromAttribute"}){	
+		#nalezeni uzlu, ktere obsahuji hledany atribut	
 		my @matchingNodes;
 		findAttrs(\%params, $xmlFile, \@matchingNodes);
 		elementSel(\%params, \$document, \$counter, @matchingNodes);
@@ -96,6 +114,8 @@ if($params{"fromElement"} eq "ROOT"){
 	}
 }
 else{
+	#nalezeni vsech elementu - pouzije se bud prvni nalezeny nebo ten s odpovidajicim
+	#atributem
 	my @allElements = $xmlFile->getElementsByTagName($params{"fromElement"});
 	if($params{"fromAttribute"}){
 		my $elem;
@@ -104,6 +124,7 @@ else{
 			my $attr;
 			foreach $attr (@attrs){
 				if($attr->toString() =~ /$params{"fromAttribute"}.*/){
+					#prvni element natvrdo prepisem...
 					$allElements[0] = $elem;
 					last;
 				}
@@ -113,14 +134,12 @@ else{
 	elementSel(\%params, \$document, \$counter, $allElements[0]);
 }
 
-if($params{"root"}){
-	
-}
-
+#vlozeni ukoncujiciho korenoveho elementu do dokumentu
 if($params{"root"}){
 	$document .= "</".$params{"root"}.">";
 }
 
+#zapis do souboru
 if($params{"output"}){
 	if(!open(OUTF, ">", $params{"output"})){
 		print STDERR "Doslo k chybe pri otevirani vystupniho souboru.\n";
@@ -132,7 +151,7 @@ if($params{"output"}){
 else {print $document."\n";}
 
 ########################################################################
-#	FUNKCE REKURZIVNIHO SESTUPU BKG
+#	FUNKCE REKURZIVNIHO SESTUPU BKG - reprezentuji jednotlive neterminaly
 ########################################################################
 sub validateQuery($){
 	my $paramsPtr = shift @_;
@@ -140,10 +159,7 @@ sub validateQuery($){
 	my $i = 0;
 	my $i_ptr = \$i;
 	
-	#foreach my $a (@words){
-	#	print $a . "\n";}
-	
-	($words[$i] eq "SELECT") or return 1;		#eq pri shode vraci 1 -> or
+	($words[$i] eq "SELECT") or return 1;	
 	$i++;
 	$$paramsPtr{"element"} = $words[$i];
 	$i++;
@@ -193,6 +209,8 @@ sub fromElement($$){
 	my @attrElem = split('\.', $words[$$i_ptr]);
 	$$paramsPtr{"fromElement"} = $attrElem[0];		
 	$$paramsPtr{"fromAttribute"} = $attrElem[1];	
+	
+	#pokud mame zadany jenom atribut,hleda se pod rootem
 	if((not $$paramsPtr{"fromElement"}) and $$paramsPtr{"fromAttribute"}){
 		($$paramsPtr{"fromElement"} = "ROOT");
 	}
@@ -231,6 +249,7 @@ sub condition($$){
 	$$paramsPtr{"whereElement"} = $attrElem[0];		
 	$$paramsPtr{"whereAttribute"} = $attrElem[1];
 	$$i_ptr++;
+	
 	relation($i_ptr, $paramsPtr) and return 1;
 	$$paramsPtr{"literal"} = $words[$$i_ptr];
 	$$i_ptr++;
@@ -257,6 +276,8 @@ sub relation($$){
 ########################################################################
 #	POMOCNE FUNKCE
 ########################################################################
+
+#rozdeleni dotazu na pole jednotlivych slov
 sub splitQuery($){
 	my $params = shift @_;
 	my $query = $$params{"query"};
@@ -275,6 +296,9 @@ sub readXML($){
 	return $fileContent;
 }
 
+########################################################################
+#	FUNKCE PRO ZPRACOVANI VLASTNIHO FILTROVANI DAT
+########################################################################
 sub findAttrs($$$){
 	my($paramsPtr, $xmlNode, $foundNodes) = @_;
 	
@@ -299,9 +323,6 @@ sub findAttrs($$$){
 	return 0;
 }
 
-########################################################################
-#	FUNKCE PRO ZPRACOVANI VLASTNIHO FILTROVANI DAT
-########################################################################
 sub elementSel($$$@){
 	my ($paramsPtr, $documentPtr, $counter, @childnodes) = @_;
 	
@@ -325,9 +346,11 @@ sub elementSel($$$@){
 
 sub evalRel($$){
 	my ($paramsPtr, $node) = @_;	
-	#podminka nezadana
+	
+	#podminka nebyla soucasti dotazu
 	$$paramsPtr{"cmp"} or return 1;
 	
+	#rozhoduje se podle obsahu elementu
 	if($$paramsPtr{"whereElement"}){
 		my @childnodes = $node->childNodes();
 		my $child;
@@ -356,5 +379,35 @@ sub evalRel($$){
 			}
 		}
 	}
-	return 1;
+	
+	#rozhoduje se podle obsahu atributu
+	if($$paramsPtr{"whereAttribute"}){
+		my @attribs = $node->attributes();
+		my $attr;
+		foreach $attr (@attribs){
+			if($attr->nodeName() eq $$paramsPtr{"whereAttribute"}){
+				if($$paramsPtr{"cmp"} eq "<") {
+					if($attr->toString() < int($$paramsPtr{"literal"})){
+						$$paramsPtr{"not"}?return 0:return 1;
+					} else { $$paramsPtr{"not"}?return 1:return 0;}
+				}
+				elsif($$paramsPtr{"cmp"} eq "="){
+					if($attr->toString() eq $$paramsPtr{"literal"}){
+						$$paramsPtr{"not"}?return 0:return 1;
+					} else { $$paramsPtr{"not"}?return 1:return 0;}
+				}
+				elsif($$paramsPtr{"cmp"} eq ">"){
+					if($attr->toString() > int($$paramsPtr{"literal"})){
+						$$paramsPtr{"not"}?return 0:return 1;
+					} else { $$paramsPtr{"not"}?return 1:return 0;}
+				}
+				elsif($$paramsPtr{"cmp"} eq "CONTAINS"){
+					if($attr->toString() =~ /.*$$paramsPtr{"literal"}.*/ ){
+						$$paramsPtr{"not"}?return 0:return 1;
+					} else { $$paramsPtr{"not"}?return 1:return 0;}
+				}
+			}
+		}
+	}
+	return 0;
 }
